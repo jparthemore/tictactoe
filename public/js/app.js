72154e5738346d32303b9efeacb9eb29385b77e4
Jan Parthemore <janparthemore@gmail.com>
2017-06-21 18:37:52 -0400

    set up board and basic app structure

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,45 @@
+/*jshint esversion: 6*/
 
+const App = {
+  rootElement: '#app',
+  numRows: 3,
+  numCols: 3,
+  cellWidth:50,
+  cellHeight:50,
+  owner: '',
+  activePlayer: 'X',
+  victoriousPlayer: 'none',//none!
+  isThereAWinner: false,
+  board: [],
+
+  start: function(){
+    this.cacheDOM();
+    this.makeBoard();
+    this.bindEvents();
+    this.render();
+
+  },
+
+  cacheDOM: function(){
+    this.root = document.querySelector(this.rootElement);
+    this.board = document.querySelector('.squares-layout');
+  },
+
+  makeBoard: function(){
+    this.board = new Array(this.numRows);
+    for(let i=0;i<this.numRows;i++){
+      this.board[i]= new Array(this.numCols);
+      for(let j=0;j<this.numCols;j++){
+        this.board[i][j]=new Square(this.cellWidth,this.cellHeight,this.owner);
+      }
+    }
+    console.log(this.board);
+  },
+  resetBoard: function(){},
+  bindEvents: function(){},
+  render: function(){},
+
+
+};
+
+App.start();
---
+++ public/js/square.js
@@ -0,0 +1,5 @@
+function Square(width, height,claimedOwner){
+  this.width = width || 100;
+  this.height = height || 100;
+  this.owner = claimedOwner || '';
+};
